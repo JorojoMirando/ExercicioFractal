790f9bd0cd9eb71bbf5f7d0b0cc9545d9c387997
jorge <jorge@newti.com.br>
2018-11-07 17:05:07 -0200

    Subindo projeto

---
+++ src/PackFractal/Fractal.java
@@ -0,0 +1,16 @@
+package PackFractal;
+
+public class Fractal {
+    private int tamanho;
+    private int interacoes;
+    private FormaGeometricaEnum forma;
+
+    public Fractal(int tamanho, int interacoes, FormaGeometricaEnum forma) {
+        this.tamanho = tamanho;
+        this.interacoes = interacoes;
+        this.forma = forma;
+    }
+
+    public static void GeraArquivoSVG(Fractal f) {
+    }
+}
